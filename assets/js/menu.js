c6cdc46b18f3c79e37cbec50fa82a6fdcea0bec9
paulpesel <paulpesel@wavedigital.cz>
2022-11-17 03:29:16 +0100

    feat: search bar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,14 @@
-$(".hamburger").on("click", function (){
+var form = $(".search-form");
+var nav = $(".nav");
+$(".hamburger").on("click", function () {
     $(this).toggleClass("is-active");
-    $(".nav").toggleClass("nav--open");
+    nav.toggleClass("nav--open");
+});
+
+$(".search").on("click", function () {
+    form.toggleClass("search--open");
+    if (nav.hasClass("nav--open")) {
+        $(".hamburger").toggleClass("is-active");
+        nav.toggleClass("nav--open");
+    }
 });
